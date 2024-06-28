f1ab6d0130c0610d59de0bf1f00b6ddfa6f4fbc2
AshwinKumar-01 <ashwinak1906@gmail.com>
2024-06-28 20:15:26 +0530

    Create Zigzag Conversion.py

---
+++ Zigzag Conversion.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s, numRows):   
+        if numRows ==1 or numRows>=len(s):
+            return s
+        else:
+            res = [''] * numRows
+            index, step =0,1
+            for char in s:
+                res[index]+=char
+                if index==0:
+                    step=1
+                elif index == numRows -1:
+                    step=-1
+                index+=step
+            return ''.join(res)
